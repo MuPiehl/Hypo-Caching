5d1d93c4932c4799aed7c7d6befda661b7d4f7a2
matthias.piehl <ma@wurst.de>
2014-03-06 22:30:24 +0100

    Scan_Activity als folge von whack_amole

--- src/de/vergleich/fedex/hypo_caching/WhackAMoleActivity.java
+++ src/de/vergleich/fedex/hypo_caching/WhackAMoleActivity.java
@@ -2,7 +2,11 @@ package de.vergleich.fedex.hypo_caching;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class WhackAMoleActivity extends Activity {
 
@@ -10,6 +14,17 @@ public class WhackAMoleActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_whack_amole);
+		
+
+		Button  button = (Button) findViewById(R.id.button_to_qr_whack);
+		
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				startActivity(new Intent(WhackAMoleActivity.this, SammelQRActivity.class));
+			}
+		});
 	}
 
 	@Override
