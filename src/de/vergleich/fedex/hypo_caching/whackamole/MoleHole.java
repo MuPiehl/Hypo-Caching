ca46df52f5bfbaad50da2de7ea1ed357e08bf7ca
Jennifer Brunklaus <jennifer.brunklaus@hypoport.de>
2014-03-07 12:54:32 +0100

    WhackaMole mit neuen Bildern und zum wegklicken

--- src/de/vergleich/fedex/hypo_caching/whackamole/MoleHole.java
+++ src/de/vergleich/fedex/hypo_caching/whackamole/MoleHole.java
@@ -1,33 +1,40 @@
 package de.vergleich.fedex.hypo_caching.whackamole;
 
-import java.util.Observable;
-
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import de.vergleich.fedex.hypo_caching.R;
 
-public class MoleHole extends Observable {
+public class MoleHole {
 
 	private final int x;
 	private final int y;
-	private final int radiusHole = 30;
-	private final int radiusMole = 20;
+	private final int radiusHole = 50;
 
 	private static final Paint HOLE = PaintColor.rgb(119, 81, 0);
-	private static final Paint MOLE = PaintColor.rgb(119, 81, 0);
 
 	private boolean active = false;
 	private int hitCount = 0;
+	private final Bitmap mole;
+	private final Bitmap hole;
 
-	public MoleHole(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public MoleHole(int x, int y, Resources resources) {
+		this.x = x + radiusHole;
+		this.y = y + radiusHole;
+		hole = BitmapFactory
+				.decodeResource(resources, R.drawable.whackamole_60);
+		mole = BitmapFactory.decodeResource(resources,
+				R.drawable.whackamole_mole60);
 	}
 
 	public void draw(Canvas canvas) {
-		canvas.drawCircle(x, y, radiusHole, HOLE);
+		canvas.drawBitmap(hole, x - 60, y - 30, HOLE);
 		if (active) {
-			canvas.drawCircle(x, y, radiusMole, MOLE);
+			canvas.drawBitmap(mole, x - 10, y - 40, HOLE);
 		}
+
 	}
 
 	public int getHitCount() {
@@ -42,16 +49,25 @@ public class MoleHole extends Observable {
 		this.active = active;
 	}
 
-	public void check(int xClick, int yClick) {
+	public boolean check(float eventX, float eventY) {
 		if (active) {
-			final double deltaX = Math.pow((x - xClick), 2);
-			final double deltaY = Math.pow((x - xClick), 2);
+			final double deltaX = Math.pow((x - eventX), 2);
+			final double deltaY = Math.pow((y - eventY), 2);
 
 			final double calcRadius = Math.sqrt(deltaX + deltaY);
 
 			if (calcRadius < radiusHole) {
 				hitCount++;
+				active = false;
+				return true;
 			}
 		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "[active=" + (active ? "true" : "false") + "  " + x + "|" + y
+				+ "]";
 	}
 }
